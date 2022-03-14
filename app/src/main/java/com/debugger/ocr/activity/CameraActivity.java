71abd244faba69af4a0625986da4960cb6a3f6e9
Rupan3th <JamseLee1981@hotmail.com>
2022-03-14 14:23:54 +0800

    recog up

--- app/src/main/java/com/debugger/ocr/activity/CameraActivity.java
+++ app/src/main/java/com/debugger/ocr/activity/CameraActivity.java
@@ -44,11 +44,8 @@ public class CameraActivity extends AppCompatActivity {
     private PreviewView cameraView;
     private TextView tvLabel;
     private LinearLayout point_Result;
-    private TextView nric_no;
-    private TextView nric_no_last4;
-    private TextView full_name;
-    private TextView address;
-    private TextView post_code;
+    private TextView block_size, nric_no, nric_no_last4, full_name, address, post_code;
+
     private EditText etOutput;
     private LinearLayout llButtons;
     private ImageView capture_btn;
@@ -58,6 +55,10 @@ public class CameraActivity extends AppCompatActivity {
     StringBuilder strBuilder1;
     ScrollView scr;
     Boolean check = false;
+    private String scan_result="";
+    private String[] scan_result_pl = {"",};
+    private String mini_scan_result="";
+    private String NRIC_NO="",fullName="",ADDRESS="",POSTCODE="";
 
     private void setupCamera() {
         ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
@@ -110,6 +111,7 @@ public class CameraActivity extends AppCompatActivity {
         cameraView = findViewById(R.id.surface_view);
         tvLabel = findViewById(R.id.tv_label);
         point_Result = findViewById(R.id.point_result);
+        block_size = findViewById(R.id.block_size);
         nric_no = findViewById(R.id.nric_no);
         nric_no_last4 = findViewById(R.id.nric_no_last4);
         full_name = findViewById(R.id.full_name);
@@ -125,7 +127,7 @@ public class CameraActivity extends AppCompatActivity {
 
         viewFinder_View = new ViewFinderView(this);
         viewFinder_View.setFrameAspectRatio((float) 0.85, (float) 1.6);
-        viewFinder_View.setFrameSize((float)0.8);
+        viewFinder_View.setFrameSize((float)0.85);
         viewFinder_View.setMaskColor(Color.parseColor("#77000000"));
         viewFinder_View.setFrameColor(Color.parseColor("#FFFFFF"));
         viewFinder_View.setFrameCornersSize(660);
@@ -167,7 +169,6 @@ public class CameraActivity extends AppCompatActivity {
 
             strBuilder.append(item.getText());
             strBuilder.append("/");
-
         }
 
         //Regex Operation
@@ -189,37 +190,103 @@ public class CameraActivity extends AppCompatActivity {
 
         }
 
-
         tvLabel.post(() -> {
-            String scan_result= strBuilder1.toString().toUpperCase();
-            try{
-                if(scan_result.indexOf("Hide detail") > 0)
-                    scan_result = scan_result.substring(0, scan_result.indexOf("Hide detail"));
-                if(scan_result.indexOf("NAME") > 0)
-                    scan_result = scan_result.substring(scan_result.indexOf("NAME"));
+            fullName = "";
+            NRIC_NO = "";
+            ADDRESS = "";
+            POSTCODE = "";
+            mini_scan_result = "";
+            scan_result = "";
 
-                String fullName = "";
-                String NRIC_NO = "";
-                if(scan_result.indexOf("NRIC") > 4){
-                    fullName = scan_result.substring(5, scan_result.indexOf("NRIC")-1);
-                    if(scan_result.indexOf("DATE OF BIR") > 0)
-                        NRIC_NO = scan_result.substring(scan_result.indexOf("NRIC")+9, scan_result.indexOf("DATE OF BIR")-1);
-                }
-                full_name.setText(fullName.replace("/", "\n"));
-                nric_no.setText(NRIC_NO);
-                nric_no_last4.setText(NRIC_NO.substring(NRIC_NO.length()-4));
-                String ADDRESS = "";
-                String POSTCODE = "";
+            String scan_builder= strBuilder1.toString().toUpperCase();
+            if(scan_builder.indexOf("REPUBLIC") > 0)
+                scan_result = scan_builder.substring(scan_builder.indexOf("REPUBLIC"));
+            else{
+                if(scan_builder.indexOf("C OF SINGA") > 0)
+                    scan_result = scan_builder.substring(scan_builder.indexOf("C OF SINGA"));
+            }
 
-                if(scan_result.indexOf("ADDRESS") > 0 && scan_result.indexOf("SINGAPORE 8") > 9 && scan_result.indexOf("ADDRESS")+9<scan_result.indexOf("SINGAPORE 8")){
-                    ADDRESS = scan_result.substring(scan_result.indexOf("ADDRESS")+8, scan_result.indexOf("SINGAPORE 8")-1);
-                    POSTCODE = scan_result.substring(scan_result.indexOf("SINGAPORE 8")+10);
+            if(scan_result.length() > 2){
+                viewFinder_View.setFrameColor(Color.parseColor("#02F868"));
+            }
+            else {
+                viewFinder_View.setFrameColor(Color.parseColor("#D50000"));
+            }
+
+            try {
+                scan_result_pl = scan_result.split("/");
+                if(scan_result_pl[2].length()<5 && (scan_result_pl[4].indexOf("(")>0 || scan_result_pl[4].indexOf(")")>0)) {
+                    fullName = scan_result_pl[3] + "\n" + scan_result_pl[4];
+                    NRIC_NO = scan_result_pl[6];
                 }
-                address.setText(ADDRESS.replace("/", "\n"));
-                post_code.setText(POSTCODE.substring(0, 6));
+                else{
+                    fullName = scan_result_pl[3];
+                    NRIC_NO = scan_result_pl[5];
+                }
+
+                block_size.setText(String.valueOf(scan_result_pl.length));
+
+                for(int i=15;i<scan_result_pl.length;i++) {
+                    if(scan_result_pl[i].indexOf("SINGAPORE")>=0){
+                        POSTCODE = scan_result_pl[i].substring(10);
+                        ADDRESS = scan_result_pl[i-2] + "\n" + scan_result_pl[i-1];
+                        if(scan_result_pl[i-3].length()>7)
+                            ADDRESS = scan_result_pl[i-3] + "\n" + scan_result_pl[i-2] + "\n" + scan_result_pl[i-1];
+                    }
+                }
+
+//                if(ADDRESS.length()>2 && POSTCODE.length()>2 && fullName.length()>2 && NRIC_NO.length()>2){
+//                    viewFinder_View.setFrameColor(Color.parseColor("#02F868"));
+//                }else{
+//                    viewFinder_View.setFrameColor(Color.parseColor("#FFFFFF"));
+//                }
+
+
             }catch (Exception e){
 
             }
+
+//            try{
+//                if(scan_result.indexOf("HIDE DE") > 0)
+//                    scan_result = scan_result.substring(0, scan_result.indexOf("HIDE DE"));
+//                if(scan_result.indexOf("NAME") > 0)
+//                    scan_result = scan_result.substring(scan_result.indexOf("NAME"));
+//                if(scan_result.indexOf("ADDRESS") > 0){
+//                    mini_scan_result = scan_result.substring(scan_result.indexOf("ADDRESS")+8);
+//                }
+//                if(scan_result.indexOf("ADCRESS") > 0) {
+//                    mini_scan_result = scan_result.substring(scan_result.indexOf("ADCRESS")+8);
+//                }
+//                if(scan_result.indexOf("ADORESS") > 0) {
+//                    mini_scan_result = scan_result.substring(scan_result.indexOf("ADORESS")+8);
+//                }
+//
+//                if(scan_result.indexOf("NRIC") > 4){
+//                    fullName = scan_result.substring(5, scan_result.indexOf("NRIC")-1);
+//                    fullName = fullName.replace("/", "\n");
+//                    if(scan_result.indexOf("DATE OF BIR") > 0)
+//                        NRIC_NO = scan_result.substring(scan_result.indexOf("NRIC")+9, scan_result.indexOf("DATE OF BIR")-1);
+//                }
+//                full_name.setText(fullName);
+//                nric_no.setText(NRIC_NO);
+//                nric_no_last4.setText(NRIC_NO.substring(NRIC_NO.length()-4));
+//
+//                if(mini_scan_result.indexOf("SlNGAPORE")>0) {
+//                    POSTCODE = mini_scan_result.substring(mini_scan_result.indexOf("SINGAPORE")+10, mini_scan_result.indexOf("SINGAPORE")+16);
+//                    ADDRESS = mini_scan_result.substring(0,mini_scan_result.indexOf("SINGAPORE")-1);
+//                    ADDRESS = ADDRESS.replace("/", "\n");
+//                }
+//
+////                if(scan_result.indexOf("ADDRESS") > 0 && scan_result.indexOf("SINGAPORE 8") > 9 && scan_result.indexOf("ADDRESS")+9<scan_result.indexOf("SINGAPORE 8")){
+////                    ADDRESS = scan_result.substring(scan_result.indexOf("ADDRESS")+8, scan_result.indexOf("SINGAPORE 8")-1);
+////                    POSTCODE = scan_result.substring(scan_result.indexOf("SINGAPORE 8")+10);
+////                }
+////                address.setText(scan_result.replace("/", "\n"));
+//                address.setText(ADDRESS);
+//                post_code.setText(POSTCODE);
+//            }catch (Exception e){
+//
+//            }
 
 
 //            tvLabel.setText(scan_result);
@@ -238,11 +305,15 @@ public class CameraActivity extends AppCompatActivity {
 //                String all_number = tvLabel.getText().toString();
 //                all_number = all_number.replace("/", "\n");
 
-                String all_number = "NRIC NO.\n"+nric_no.getText().toString()+"\n"+
-                                    nric_no_last4.getText().toString()+"\n"+ "\nName:\n"+
-                                    full_name.getText().toString()+"\n"+ "\nAddress:\n"+
-                                    address.getText().toString()+"\n"+ "\nPostal Code:\n"+
-                                    post_code.getText().toString();
+                String NRIC_NO_lst4 = "";
+                if(NRIC_NO.length()>4) NRIC_NO_lst4 = NRIC_NO.substring(NRIC_NO.length()-4);
+                POSTCODE = POSTCODE.replace("]", "1");
+                POSTCODE = POSTCODE.replace(")", "1");
+
+                String all_number = "NRIC NO.\n"+NRIC_NO+"\n"+NRIC_NO_lst4+"\n"+
+                                    "\nName:\n"+fullName+"\n"+
+                                    "\nAddress:\n"+ADDRESS+"\n"+
+                                    "\nPostal Code:\n"+POSTCODE;
 
                 etOutput.setText(all_number);
                 etOutput.requestFocus();
